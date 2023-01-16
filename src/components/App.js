317aeb96a0e1c0f4bd85c3753b17d3b58fc40de2
Nupur Gohite <118847362+gohitenupur@users.noreply.github.com>
2023-01-16 22:56:54 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,26 @@ import React from 'react'
 import '../styles/App.css';
 
 class App extends React.Component {
-  
+   constructor(){
+    super();
+    this.state={
+      name:"John Doe",
+      enrollmentNo: "12345678",
+      age: 34 
+    }
+  }
+  render(){
+    return(
+    <>
+      <p>Hello, my name is {name} <h1> and 'I am {age} years old and my enrollment no is {enrollmentNo}<h1></p>
+      <button onClick={() => {
+            this.setState({
+              age: this.state.age + 1,
+            });
+          }}>Increase age</button>
+    </>
+    )
+  }
 }
 
 
