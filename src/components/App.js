90fb92cada3ac225375a06dda1127a2acead4a41
Nupur Gohite <118847362+gohitenupur@users.noreply.github.com>
2023-01-20 11:31:55 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,24 @@
-import React, { useState } from 'react';
-import React from 'react'
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      name: "John Doe",
-      enrollmentNo: "12345678",
-      age: 34
-    };
+    this.state = { name: "John Doe", enrollmentNo: "12345678", age: 34 };
   }
- }
-  handleClick = () => {
-    this.setState(prevState => ({
-      age: prevState.age + 1
-    }));
-  }
+  incrementNumber = () => {
+    this.setState({ age: this.state.age + 1 });
+  };
 
   render() {
     return (
       <div>
         <h1>Hello, my name is {this.state.name}</h1>
-        <p>I am {this.state.age} years old and my enrollment no is {this.state.enrollmentNo}</p>
-        <button onClick={this.handleClick}>Increase Age</button>
+        <p>
+          I am {this.state.age} years old and my enrollment no is{" "}
+          {this.state.enrollmentNo}
+        </p>
+        <button onClick={this.incrementNumber}>Increment</button>
       </div>
     );
   }
