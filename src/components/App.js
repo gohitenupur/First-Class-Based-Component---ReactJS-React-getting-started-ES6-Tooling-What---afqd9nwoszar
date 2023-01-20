59140f7d439b8b82801e7ee4a5685c27c70ffa31
Nupur Gohite <118847362+gohitenupur@users.noreply.github.com>
2023-01-20 11:15:04 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,39 @@
+import React, { useState } from 'react';
 import React from 'react'
 import '../styles/App.css';
 
 class App extends React.Component {
-   constructor(){
-    super();
-    this.state={
-      name:"John Doe",
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "John Doe",
       enrollmentNo: "12345678",
-      age: 34 
-    }
+      age: 34
+    };
   }
-  render(){
-    return(
-    <>
-      <p>Hello, my name is <h1> {this.state.name} </h1> and 'I am <h1>{this.state.age}</h1> years old and my enrollment no is <h1>{this.state.enrollmentNo}</h1></p>
-      <button onClick={() => {
-            this.setState({
-              age: this.state.age + 1,
-            });
-          }}>Increase age</button>
-    </>
-    )
+ }
+  handleClick = () => {
+    this.setState(prevState => ({
+      age: prevState.age + 1
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, my name is {this.state.name}</h1>
+        <p>I am {this.state.age} years old and my enrollment no is {this.state.enrollmentNo}</p>
+        <button onClick={this.handleClick}>Increase Age</button>
+      </div>
+    );
   }
 }
 
-
 export default App;
+
+
+
+
+ 
+
+ 
